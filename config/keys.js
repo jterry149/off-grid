9a641ebb3945b45b42596e6af3cdeeb19b666f40
JTerry <jessica.terry79@yahoo.com>
2019-01-22 18:14:47 -0600

    added comments to the config keys javascript files

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
+// If else statement to produce the correct keys for production environment and development environment
 if (process.env.NODE_ENV === 'production')
 {
     module.exports = require('./keys_prod');
