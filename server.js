66b75f4e430a0c526d808e04f5ebd4e685bbc0e3
JTerry <jessica.terry79@yahoo.com>
2019-01-22 18:04:03 -0600

    got the database connected and working and established testing routes

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
-// Dependencies
+// Required dependencies and files 
 const express = require('express');
+const mongoose = require('mongoose');
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
 
 // Variable initialized for express
 const app = express();
 
+// Datbase Config
+const db = require('./config/keys').MONGO_URI;
+
+// Connect to MongoDB
+mongoose.connect(db, { useNewUrlParser: true })
+.then(() => console.log("mongoDB connected"))
+.catch(err => console.log(err));;
+    
+
 // Initial testing route
-app.get('/', function(res,res)  
-{
-    res.send('Hello World');
-});
+app.get('/', (req,res) => res.send('Hello World'));
+
+// Request go through our middleware routes
+app.use(users);
+app.use(profile);
+app.use(posts);
 
 // Variable to set the port
 const port = process.env.PORT
