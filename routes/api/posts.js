e9a21783b9a275aa9e14da9170d6d004c9908d5f
JTerry <jessica.terry79@yahoo.com>
2019-01-22 19:20:43 -0600

    updating post routes connection to router

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 // Get method to test post 
-router.get('/postTest', (req, res) => res.json({ msg: "Posts works" }));
+router.get('/postTest', (req, res) => res.json({ msg: "Posts works just fine" }));
 
 // Export the router
 module.exports = router;
