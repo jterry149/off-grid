4d703599b2250fc618fcf45c67ded0f79eda50a5
JTerry <jessica.terry79@yahoo.com>
2019-01-24 22:14:39 -0600

    added login route to users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,12 +6,12 @@ const bycrypt = require('bcryptjs');
 const User = require('../../models/User');
 
 // Route:       GET api/users/test
-// Description: Tests users route
+// Description: Tests users route functioning
 // Access:      Public
 router.get('/usersTest', (req, res) => res.json({ msg: "Users test works" }));
 
 // Route:       GET api/users/register
-// Description: Register user
+// Description: Register user and POST to the database
 // Access:      Public
 router.post('/register', (req, res) => {
     // Find user email if exists if not create the user in the database
@@ -22,7 +22,7 @@ router.post('/register', (req, res) => {
         } else {
             // Grab the email address assigned avatar or set a default icon
             const avatar = gravatar.url(req.body.email, {
-                size: '200',  // size of avatar
+                size: '175',  // size of avatar
                 rating: 'pg', // rating of avatar
                 default: 'mm' // default icon
             });
@@ -51,6 +51,33 @@ router.post('/register', (req, res) => {
     })
 });
 
+// Route:       GET api/users/login
+// Description: Login user then return Json Web tokens
+// Access:      Public
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    // Find the user by email address
+    User.findOne({email})
+        .then(user => {
+            // If statement to check for user if not found then return not found
+            if(!user){
+                return res.status(404).json({email: 'User was not found'});
+            }
 
+            // Check the password using bycrypt by comparing the hashed password with the actual password passed.
+            bycrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if(isMatch) {
+                        res.json({msg: 'Success'});
+                    } else {
+                        return res.status(400).json({password: "Password was incorrect"});
+                    }
+                })
+
+        });
+});
+
+    
 // Export the router
 module.exports = router;
