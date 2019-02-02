b2c767f7c8e11d141a69634ddad72cc266481599
Rebecca Baker <rebecca122@mac.com>
2019-02-02 00:57:36 -0600

    login progress

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -4,13 +4,7 @@ function Card() {
   return (
     <div className="card">
       <div className="card-body">
-        <p className="card-text">
-          Aliquip dolore commodo nostrud minim. Cillum do enim non ullamco. Commodo magna eu ex
-          mollit sunt amet fugiat. In irure eu enim id ea sit nostrud incididunt ad
-          adipisicing.Aliquip dolore commodo nostrud minim. Cillum do enim non ullamco. Commodo
-          magna eu ex mollit sunt amet fugiat. In irure eu enim id ea sit nostrud incididunt ad
-          adipisicing.
-        </p>
+        <p className="card-text">Marketing Links - Text - Options</p>
       </div>
     </div>
   );
---
+++ client/src/components/Login/Login.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import {
+  Container,
+  Col,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Button
+} from "reactstrap";
+import "./styles.css";
+
+class Login extends Component {
+  render() {
+    return (
+      <Container className="App">
+        <h2>Sign In</h2>
+        <Form className="form">
+          <Col>
+            <FormGroup>
+              <Label>Email</Label>
+              <Input
+                type="email"
+                name="email"
+                id="exampleEmail"
+                placeholder="myemail@email.com"
+              />
+            </FormGroup>
+          </Col>
+          <Col>
+            <FormGroup>
+              <Label for="examplePassword">Password</Label>
+              <Input
+                type="password"
+                name="password"
+                id="examplePassword"
+                placeholder="********"
+              />
+            </FormGroup>
+          </Col>
+          <Button>Submit</Button>
+        </Form>
+      </Container>
+    );
+  }
+}
+
+export default Login;
