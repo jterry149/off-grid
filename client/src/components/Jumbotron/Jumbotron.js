6908c7993f38a74c56e79fabb77fe5e86f56ce68
Rebecca Baker <rebecca122@mac.com>
2019-02-11 00:33:20 -0600

    style updates and navbar

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -1,17 +1,24 @@
 // Required Dependecies 
 import React from "react";
 import { MDBJumbotron, MDBContainer } from "mdbreact";
+import Logo from "./traveLife_logo.png";
+import Warrior from "../../images/worrier.png";
 
 const Jumbotron = () => {
   return (
     <MDBJumbotron fluid className="text-center jumbotron">
       <MDBContainer>
-        <h1>TraveLife Road Warriors</h1>
-          <p>
-            Your home is where the open road will lead you. 
+        {/* <h1>TraveLife Road Warriors</h1> */}
+        <img src={Logo} className="rounded mx-auto d-block" alt="logo" />
+        <p>
+          Home is where the open road leads you.
             <br />
-            Discover everything you need to becoming a TraveLife Road Warrior.
+          Discover everything you need to become a TraveLife Road Warrior.
+
           </p>
+        <p>
+          <img src={Warrior} className="rounded mx-auto d-block" alt="warrior" />
+        </p>
       </MDBContainer>
     </MDBJumbotron>
   );
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,17 +7,17 @@ import Card from "../components/Card/Card";
 
 class Home extends Component {
 
-render() {
-    return(
-        <div className='Site'>
-            <div className='Content'>
-                <Jumbotron />
-                {/* <Carousel /> */}
-                <Card />
+    render() {
+        return (
+            <div className='Site'>
+                <div className='Content'>
+                    <Jumbotron />
+                    {/* <Carousel /> */}
+                    {/* <Card /> */}
+                </div>
             </div>
-      </div>  
-    );
-}
+        );
+    }
 }
 
 export default Home;
