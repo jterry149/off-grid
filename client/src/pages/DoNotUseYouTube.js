e417557e75e8200b5c669cd143c36844ca412cac
Rebecca Baker <rebecca122@mac.com>
2019-02-13 02:09:14 -0600

    finished youtube

---
+++ client/src/pages/DoNotUseYouTube.js
@@ -0,0 +1,15 @@
+// import React from 'react';
+// import YouTubeCard from '../components/YouTube/YouTubeCard';
+// import videos from '../utils/videos.json';
+
+// const CardExample = () => {
+//   return (
+//     <div className="youtubetitle">
+//       <h1>Resources for Living on the Road</h1>
+
+//       {videos.map(video => <YouTubeCard videoUrl={video.url} />)}
+//     </div>
+//   )
+// }
+
+// export default CardExample;
